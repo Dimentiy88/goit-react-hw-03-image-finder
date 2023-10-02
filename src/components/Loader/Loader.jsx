79180c051a795dedc90dfbb4efba4921ea157d58
Dimentiy88 <bodartinov88@gmail.com>
2023-10-02 17:09:35 +0200

    hw-03-images

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import css from './Loader.module.css';
+import { RotatingLines } from 'react-loader-spinner';
+export const Loader = () => (
+  <div className={css.Loader}>
+    <RotatingLines
+      strokeColor="grey"
+      strokeWidth="10"
+      animationDuration="0.75"
+      width="96"
+      visible={true}
+    />
+  </div>
+);
